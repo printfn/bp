c3fc9d1be0c77d97e95e58074e46f8a4038474d7
printfn <printfn@users.noreply.github.com>
2022-04-14 05:54:02 +0200

    Clean up code

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
-use std::io::{Read, Write};
+use std::{
+    error,
+    io::{self, Write},
+    process,
+};
+
+pub type Error = Box<dyn error::Error + Send + Sync + 'static>;
 
 use copypasta::{ClipboardContext, ClipboardProvider};
-
-// based on https://gist.github.com/RichardBronosky/56d8f614fab2bacdd8b048fb58d0c0c7
 
 fn stdin_tty() -> bool {
     atty::is(atty::Stream::Stdin)
@@ -12,25 +16,53 @@ fn stdout_tty() -> bool {
     atty::is(atty::Stream::Stdout)
 }
 
-fn paste() {
-    let mut ctx = ClipboardContext::new().unwrap();
-    let contents = ctx.get_contents().unwrap();
-    std::io::stdout().write(contents.as_bytes()).unwrap();
-    if !contents.ends_with('\n') && stdout_tty() {
-        println!();
-    }
+fn copy(mut file: impl io::Read) -> Result<Vec<u8>, Error> {
+    let mut ctx = ClipboardContext::new()?;
+    let mut contents = Vec::with_capacity(32);
+    file.read_to_end(&mut contents)?;
+    ctx.set_contents(String::from_utf8(contents.clone())?)?;
+    Ok(contents)
 }
 
-fn copy() {
-    let mut ctx = ClipboardContext::new().unwrap();
-    let mut string = String::new();
-    std::io::stdin().read_to_string(&mut string).unwrap();
-    ctx.set_contents(string).unwrap();
+fn paste() -> Result<String, Error> {
+    let mut ctx = ClipboardContext::new()?;
+    let contents = ctx.get_contents()?;
+    Ok(contents)
+}
+
+fn inner_main() -> Result<(), Error> {
+    match (stdin_tty(), stdout_tty()) {
+        (true, true) => {
+            let contents = paste()?;
+            print!("{}", contents);
+            if !contents.ends_with('\n') {
+                println!();
+            }
+        }
+        (true, false) => {
+            print!("{}", paste()?);
+        }
+        (false, true) => {
+            copy(io::stdin())?;
+        }
+        (false, false) => {
+            let contents = copy(io::stdin())?;
+            io::stdout().write_all(&contents)?;
+        }
+    }
+    Ok(())
 }
 
 fn main() {
-    if !stdin_tty() {
-        copy()
+    match inner_main() {
+        Ok(()) => (),
+        Err(e) => {
+            eprint!("{}", e);
+            while let Some(e) = e.source() {
+                eprint!(": {}", e);
+            }
+            eprintln!();
+            process::exit(1);
+        }
     }
-    paste()
 }
