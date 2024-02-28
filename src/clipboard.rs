a6cd4c7539c826938d84417bbff8572f35f97437
printfn <printfn@users.noreply.github.com>
2024-02-28 09:12:09 +0000

    Use copypasta_ext

--- src/clipboard.rs
+++ src/clipboard.rs
@@ -1,26 +1,27 @@
 use super::Error;
 use std::io;
 
-use copypasta::{ClipboardContext, ClipboardProvider};
+use copypasta_ext::prelude::*;
+use copypasta_ext::x11_fork::ClipboardContext;
 
 pub fn copy(mut file: impl io::Read, strip_whitespace: bool) -> Result<Vec<u8>, Error> {
-    let mut ctx = ClipboardContext::new()?;
-    let mut contents = Vec::with_capacity(32);
-    file.read_to_end(&mut contents)?;
+	let mut ctx = ClipboardContext::new()?;
+	let mut contents = Vec::with_capacity(32);
+	file.read_to_end(&mut contents)?;
 
-    let mut contents = String::from_utf8(contents)?;
-    if strip_whitespace {
-        contents = contents.trim().to_string();
-    }
-    ctx.set_contents(contents.clone())?;
-    Ok(contents.into())
+	let mut contents = String::from_utf8(contents)?;
+	if strip_whitespace {
+		contents = contents.trim().to_string();
+	}
+	ctx.set_contents(contents.clone())?;
+	Ok(contents.into())
 }
 
 pub fn paste(strip_whitespace: bool) -> Result<Vec<u8>, Error> {
-    let mut ctx = ClipboardContext::new()?;
-    let mut contents = ctx.get_contents()?;
-    if strip_whitespace {
-        contents = contents.trim().to_string();
-    }
-    Ok(contents.into_bytes())
+	let mut ctx = ClipboardContext::new()?;
+	let mut contents = ctx.get_contents()?;
+	if strip_whitespace {
+		contents = contents.trim().to_string();
+	}
+	Ok(contents.into_bytes())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::{
-    env, error, ffi, fmt, fs,
-    io::{self, Write},
-    process,
+	env, error, ffi, fmt, fs,
+	io::{self, Write},
+	process,
 };
 
 mod clipboard;
@@ -9,94 +9,94 @@ mod clipboard;
 pub type Error = Box<dyn error::Error + Send + Sync + 'static>;
 
 fn stdin_tty() -> bool {
-    io::IsTerminal::is_terminal(&io::stdin())
+	io::IsTerminal::is_terminal(&io::stdin())
 }
 
 fn stdout_tty() -> bool {
-    io::IsTerminal::is_terminal(&io::stdout())
+	io::IsTerminal::is_terminal(&io::stdout())
 }
 
 #[derive(Debug)]
 struct UnknownArgumentError(ffi::OsString);
 impl fmt::Display for UnknownArgumentError {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "unknown argument `{}`", self.0.to_string_lossy())
-    }
+	fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+		write!(f, "unknown argument `{}`", self.0.to_string_lossy())
+	}
 }
 impl error::Error for UnknownArgumentError {}
 
 fn inner_main() -> Result<(), Error> {
-    let mut input_file = None;
-    let mut force_stdout = false;
-    let mut output_to_stderr = false;
-    let mut strip_whitespace = false;
-    for arg in env::args_os().skip(1) {
-        if arg == "-h" || arg == "--help" {
-            print_usage(io::stdout())?;
-            return Ok(());
-        } else if arg == "--version" || arg == "-V" || arg == "-v" {
-            print_version()?;
-            return Ok(());
-        } else if arg == "-O" || arg == "--stdout" {
-            force_stdout = true;
-        } else if arg == "-E" || arg == "--stderr" {
-            output_to_stderr = true;
-        } else if arg == "-s" || arg == "--strip" {
-            strip_whitespace = true;
-        } else if arg
-            .as_os_str()
-            .to_str()
-            .map_or(false, |s| s.starts_with('-'))
-        {
-            return Err(UnknownArgumentError(arg))?;
-        } else if input_file.is_none() {
-            input_file = Some(arg);
-        } else {
-            return Err(UnknownArgumentError(arg))?;
-        }
-    }
+	let mut input_file = None;
+	let mut force_stdout = false;
+	let mut output_to_stderr = false;
+	let mut strip_whitespace = false;
+	for arg in env::args_os().skip(1) {
+		if arg == "-h" || arg == "--help" {
+			print_usage(io::stdout())?;
+			return Ok(());
+		} else if arg == "--version" || arg == "-V" || arg == "-v" {
+			print_version()?;
+			return Ok(());
+		} else if arg == "-O" || arg == "--stdout" {
+			force_stdout = true;
+		} else if arg == "-E" || arg == "--stderr" {
+			output_to_stderr = true;
+		} else if arg == "-s" || arg == "--strip" {
+			strip_whitespace = true;
+		} else if arg
+			.as_os_str()
+			.to_str()
+			.map_or(false, |s| s.starts_with('-'))
+		{
+			return Err(UnknownArgumentError(arg))?;
+		} else if input_file.is_none() {
+			input_file = Some(arg);
+		} else {
+			return Err(UnknownArgumentError(arg))?;
+		}
+	}
 
-    let mut tty_paste = false;
+	let mut tty_paste = false;
 
-    let contents = if let Some(input_file) = input_file {
-        let file = fs::File::open(input_file)?;
-        clipboard::copy(file, strip_whitespace)?
-    } else if stdin_tty() {
-        if !output_to_stderr {
-            tty_paste = true;
-        }
-        clipboard::paste(strip_whitespace)?
-    } else {
-        clipboard::copy(io::stdin(), strip_whitespace)?
-    };
+	let contents = if let Some(input_file) = input_file {
+		let file = fs::File::open(input_file)?;
+		clipboard::copy(file, strip_whitespace)?
+	} else if stdin_tty() {
+		if !output_to_stderr {
+			tty_paste = true;
+		}
+		clipboard::paste(strip_whitespace)?
+	} else {
+		clipboard::copy(io::stdin(), strip_whitespace)?
+	};
 
-    if force_stdout || tty_paste || !stdout_tty() {
-        io::stdout().write_all(&contents)?;
-        if tty_paste
-            && !contents.ends_with(&[b'\n'])
-            && !contents.is_empty()
-            && stdout_tty()
-            && !strip_whitespace
-        {
-            println!();
-        }
-    }
+	if force_stdout || tty_paste || !stdout_tty() {
+		io::stdout().write_all(&contents)?;
+		if tty_paste
+			&& !contents.ends_with(&[b'\n'])
+			&& !contents.is_empty()
+			&& stdout_tty()
+			&& !strip_whitespace
+		{
+			println!();
+		}
+	}
 
-    if output_to_stderr {
-        io::stderr().write_all(&contents)?;
-    }
+	if output_to_stderr {
+		io::stderr().write_all(&contents)?;
+	}
 
-    Ok(())
+	Ok(())
 }
 
 fn get_version_number() -> &'static str {
-    env!("CARGO_PKG_VERSION")
+	env!("CARGO_PKG_VERSION")
 }
 
 fn print_usage(mut f: impl io::Write) -> io::Result<()> {
-    write!(
-        f,
-        "Usage: bp [FLAGS] [file]
+	write!(
+		f,
+		"Usage: bp [FLAGS] [file]
 
 Version {version}
 
@@ -106,26 +106,26 @@ Flags:
     -O  --stdout    print clipboard contents to stdout
     -E  --stderr    print clipboard contents to stderr
     -s  --strip     strip whitespace from the content\n",
-        version = get_version_number()
-    )?;
-    Ok(())
+		version = get_version_number()
+	)?;
+	Ok(())
 }
 
 fn print_version() -> io::Result<()> {
-    writeln!(io::stdout(), "bp {}", get_version_number())?;
-    Ok(())
+	writeln!(io::stdout(), "bp {}", get_version_number())?;
+	Ok(())
 }
 
 fn main() {
-    match inner_main() {
-        Ok(()) => (),
-        Err(e) => {
-            eprint!("Error: {}", e);
-            while let Some(e) = e.source() {
-                eprint!(": {}", e);
-            }
-            eprintln!();
-            process::exit(1);
-        }
-    }
+	match inner_main() {
+		Ok(()) => (),
+		Err(e) => {
+			eprint!("Error: {}", e);
+			while let Some(e) = e.source() {
+				eprint!(": {}", e);
+			}
+			eprintln!();
+			process::exit(1);
+		}
+	}
 }
